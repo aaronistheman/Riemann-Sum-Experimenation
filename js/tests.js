5538a42a3b107c1bae6df020449b9f2b4c9ac71e
aaronistheman <aarons.7007@gmail.com>
2016-06-02 13:41:03 -0700

    Made second riemann sum test; may not be too good
    I've been encountering some issues with QUnit, so I might have to change
    the way the tests are organized.

--- js/tests.js
+++ js/tests.js
@@ -1,26 +1,29 @@
 "use strict";
-
-QUnit.module("riemann.js, RiemannGenerator constructor");
-
-
   
 QUnit.module("riemann.js, RiemannGenerator.getLeftRiemannSum()");
 
-  QUnit.test("test 1: linear function", function(assert) {
-    var rg = new RiemannGenerator(function(x) {
-      return 2 * x + 3;
-    }, 5);
+  // QUnit.test("test 1: linear function", function(assert) {
+    // var rg = new RiemannGenerator(function(x) {
+      // return 2 * x + 3;
+    // }, 5);
     
-    var RESULT_CALCULATED_BY_HAND = 35;
-    assert.deepEqual(rg.getLeftRiemannSum(0, 5), RESULT_CALCULATED_BY_HAND);
-  }); // test 1
+    // var RESULT_CALCULATED_BY_HAND = 35;
+    // assert.deepEqual(rg.getLeftRiemannSum(0, 5), RESULT_CALCULATED_BY_HAND,
+      // "Correct calculation");
+      
+  // }); // test 1
 
   QUnit.test("test 2: linear function; harder calculation", function(assert) {
-    var rg = new RiemannGenerator(function(x) {
+    var func = function(x) {
       return 2 * x + 3;
-    }, 100);
+    };
+    var rg = new RiemannGenerator(func, 100);
     
-    
+    var range = 5 - (-5);
+    var step = range / 100;
+    var answer = 0;
+    for (var x = -5; x < 5; x += step)
+      answer += func(x) * step;
     
     assert.deepEqual(rg.getLeftRiemannSum(-5, 5), answer);
     assert.notOk(false);
