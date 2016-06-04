18dae270857b2298ddc5eee0b155f918ef5ef3ac
aaronistheman <aarons.7007@gmail.com>
2016-06-04 14:19:24 -0700

    Added what I feel are the best tests for the Riemann functions
    Testing a mathematical formula by using the same mathematical formula
    seems silly, so I'm not testing for correct output.

--- js/tests.js
+++ js/tests.js
@@ -2,59 +2,24 @@
   
 QUnit.module("riemann.js, RiemannGenerator.getLeftRiemannSum()");
 
-  QUnit.test("test 1: linear function", function(assert) {
-    var rg = new RiemannGenerator(function(x) {
-      return 2 * x + 3;
-    }, 5);
-    
-    var RESULT_CALCULATED_BY_HAND = 35;
-    assert.deepEqual(rg.getLeftRiemannSum(0, 5), RESULT_CALCULATED_BY_HAND,
-      "Correct calculation");
-      
-  }); // test 1
-
-  QUnit.test("test 2: linear function; harder calculation", function(assert) {
-    var func = function(x) {
-      return 2 * x + 3;
-    };
-    var rg = new RiemannGenerator(func, 100);
-    
-    var range = 5 - (-5);
-    var step = range / 100;
-    var answer = 0;
-    for (var x = -5; x < 5; x += step)
-      answer += func(x) * step;
-    
-    assert.deepEqual(rg.getLeftRiemannSum(-5, 5), answer);
-    assert.notOk(false);
-  }); // test 2
-  
-  // QUnit.test("test 3: quadratic function", function(assert) {
-    
-  // }); // test 3
-
-QUnit.module("riemann.js, RiemannGenerator.getRightRiemannSum()");
-
-  QUnit.test("test 1: linear function", function(assert) {
-    var rg = new RiemannGenerator(function(x) {
-      return 2 * x + 3;
-    }, 5);
-    
-    var RESULT_CALCULATED_BY_HAND = 45;
-    assert.deepEqual(rg.getRightRiemannSum(0, 5), RESULT_CALCULATED_BY_HAND);
-  }); // test 1
-
-  QUnit.test("test 2: linear function; harder calculation", function(assert) {
+  QUnit.test("left riemann is lower than real integral", function(assert) {
     var rg = new RiemannGenerator(function(x) {
       return 2 * x + 3;
     }, 100);
     
+    var HAND_CALCULATED_INTEGRAL_ANSWER = 40;
     
+    assert.ok(rg.getLeftRiemannSum(0, 5) < HAND_CALCULATED_INTEGRAL_ANSWER);
+  });
+
+QUnit.module("riemann.js, RiemannGenerator.getRightRiemannSum()");
+
+  QUnit.test("right riemann is higher than real integral", function(assert) {
+    var rg = new RiemannGenerator(function(x) {
+      return 2 * x + 3;
+    }, 100);
     
-    assert.deepEqual(rg.getRightRiemannSum(-5, 5), answer);
-    assert.notOk(false);
-  }); // test 2
-  
-  // QUnit.test("test 3: quadratic function", function(assert) {
+    var HAND_CALCULATED_INTEGRAL_ANSWER = 40;
     
-  // }); // test 3
+    assert.ok(rg.getRightRiemannSum(0, 5) > HAND_CALCULATED_INTEGRAL_ANSWER);
+  });
--- open.sh
+++ open.sh
@@ -8,4 +8,4 @@ css/index.css \
 todo-list.txt \
 js/riemann.js \
 test.html \
-tests.js \
+js/tests.js \
