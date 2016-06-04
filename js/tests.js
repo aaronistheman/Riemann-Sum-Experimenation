89c9bd0ba5905f85ea8b36595038318f2cd7a5fc
aaronistheman <aarons.7007@gmail.com>
2016-06-04 15:01:20 -0700

    Added the opposite of each existing test
    That is, I added tests that use -2x + 3 instead of 2x + 3.

--- js/tests.js
+++ js/tests.js
@@ -19,6 +19,23 @@ QUnit.module("riemann.js, RiemannGenerator.getLeftRiemannSum()");
     }
   });
 
+  QUnit.test("left riemann is higher than real integral", function(assert) {
+    var numRects = [10, 100, 500, 1000, 5000];
+    
+    var rg = new RiemannGenerator(function(x) {
+      return -2 * x + 3;
+    }, 3);
+    
+    var HAND_CALCULATED_INTEGRAL_ANSWER = 40;
+    
+    for (var i = 0; i < numRects.length; ++i)
+    {
+      rg.setNumRectangles(numRects[i]);
+      assert.ok(rg.getLeftRiemannSum(-5, 0) > HAND_CALCULATED_INTEGRAL_ANSWER,
+        "Correct for " + numRects[i] + " rectangles");
+    }
+  });
+
 QUnit.module("riemann.js, RiemannGenerator.getRightRiemannSum()");
 
   QUnit.test("right riemann is higher than real integral", function(assert) {
@@ -26,7 +43,7 @@ QUnit.module("riemann.js, RiemannGenerator.getRightRiemannSum()");
     
     var rg = new RiemannGenerator(function(x) {
       return 2 * x + 3;
-    }, 100);
+    }, 3);
     
     var HAND_CALCULATED_INTEGRAL_ANSWER = 40;
     
@@ -34,6 +51,23 @@ QUnit.module("riemann.js, RiemannGenerator.getRightRiemannSum()");
     {
       rg.setNumRectangles(numRects[i]);
       assert.ok(rg.getRightRiemannSum(0, 5) > HAND_CALCULATED_INTEGRAL_ANSWER,
+        "Correct for " + numRects[i] + " rectangles");
+    }
+  });
+
+  QUnit.test("right riemann is lower than real integral", function(assert) {
+    var numRects = [10, 100, 500, 1000, 5000];
+    
+    var rg = new RiemannGenerator(function(x) {
+      return -2 * x + 3;
+    }, 3);
+    
+    var HAND_CALCULATED_INTEGRAL_ANSWER = 40;
+    
+    for (var i = 0; i < numRects.length; ++i)
+    {
+      rg.setNumRectangles(numRects[i]);
+      assert.ok(rg.getRightRiemannSum(-5, 0) < HAND_CALCULATED_INTEGRAL_ANSWER,
         "Correct for " + numRects[i] + " rectangles");
     }
   });
