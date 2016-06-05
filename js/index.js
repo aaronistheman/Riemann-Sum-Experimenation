b269b555fcee4a3d4a49dfc7a33a0f72eb5f1582
aaronistheman <aarons.7007@gmail.com>
2016-06-05 10:29:21 -0700

    Made custom type for data associated with menu option

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
 "use strict";
 
-$(document).ready(function() {
+// I treat OptionData like a C++ struct
+function OptionData(displayHeader, func, rangeLow, rangeHigh)
+{
+  this.displayHeader = displayHeader;
+  this.func = func;
+  this.rangeLow = rangeLow;
+  this.rangeHigh = rangeHigh;
+  
+  var rg = new RiemannGenerator(func, 500000);
+  this.leftSum = rg.getLeftRiemannSum(rangeLow, rangeHigh);
+  this.rightSum = rg.getRightRiemannSum(rangeLow, rangeHigh);
+} // custom type OptionData
+
+OptionData.NUM_RECTANGLES = 500000;
+
+$(document).ready(function()
+{
   $("#lin-button").click(function() {
     alert($(this).html());
   });
