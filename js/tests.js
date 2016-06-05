0a203c26cab7e6bd6af9ed1dde0101b21f7a0257
aaronistheman <aarons.7007@gmail.com>
2016-06-04 22:52:10 -0700

    Figured out good number of rectangles to use
    By "good", I mean not taking too much time. I could still increase it, in
    fact. QUnit tells me that calculating a Riemann sum with 500,000
    rectangles can't take more than 65 ms.

--- js/tests.js
+++ js/tests.js
@@ -2,72 +2,66 @@
   
 QUnit.module("riemann.js, RiemannGenerator.getLeftRiemannSum()");
 
+  var numRectsArray = [10, 100, 500, 1000, 5000, 10000, 500000, 1000000];
+
   QUnit.test("left riemann is lower than real integral", function(assert) {
-    var numRects = [10, 100, 500, 1000, 5000];
-    
     var rg = new RiemannGenerator(function(x) {
       return 2 * x + 3;
     }, 3);
     
     var HAND_CALCULATED_INTEGRAL_ANSWER = 40;
     
-    for (var i = 0; i < numRects.length; ++i)
+    for (var i = 0; i < numRectsArray.length; ++i)
     {
-      rg.setNumRectangles(numRects[i]);
+      rg.setNumRectangles(numRectsArray[i]);
       assert.ok(rg.getLeftRiemannSum(0, 5) < HAND_CALCULATED_INTEGRAL_ANSWER,
-        "Correct for " + numRects[i] + " rectangles");
+        "Correct for " + numRectsArray[i] + " rectangles");
     }
   });
 
   QUnit.test("left riemann is higher than real integral", function(assert) {
-    var numRects = [10, 100, 500, 1000, 5000];
-    
     var rg = new RiemannGenerator(function(x) {
       return -2 * x + 3;
     }, 3);
     
     var HAND_CALCULATED_INTEGRAL_ANSWER = 40;
     
-    for (var i = 0; i < numRects.length; ++i)
+    for (var i = 0; i < numRectsArray.length; ++i)
     {
-      rg.setNumRectangles(numRects[i]);
+      rg.setNumRectangles(numRectsArray[i]);
       assert.ok(rg.getLeftRiemannSum(-5, 0) > HAND_CALCULATED_INTEGRAL_ANSWER,
-        "Correct for " + numRects[i] + " rectangles");
+        "Correct for " + numRectsArray[i] + " rectangles");
     }
   });
 
 QUnit.module("riemann.js, RiemannGenerator.getRightRiemannSum()");
 
   QUnit.test("right riemann is higher than real integral", function(assert) {
-    var numRects = [10, 100, 500, 1000, 5000];
-    
     var rg = new RiemannGenerator(function(x) {
       return 2 * x + 3;
     }, 3);
     
     var HAND_CALCULATED_INTEGRAL_ANSWER = 40;
     
-    for (var i = 0; i < numRects.length; ++i)
+    for (var i = 0; i < numRectsArray.length; ++i)
     {
-      rg.setNumRectangles(numRects[i]);
+      rg.setNumRectangles(numRectsArray[i]);
       assert.ok(rg.getRightRiemannSum(0, 5) > HAND_CALCULATED_INTEGRAL_ANSWER,
-        "Correct for " + numRects[i] + " rectangles");
+        "Correct for " + numRectsArray[i] + " rectangles");
     }
   });
 
   QUnit.test("right riemann is lower than real integral", function(assert) {
-    var numRects = [10, 100, 500, 1000, 5000];
-    
     var rg = new RiemannGenerator(function(x) {
       return -2 * x + 3;
     }, 3);
     
     var HAND_CALCULATED_INTEGRAL_ANSWER = 40;
     
-    for (var i = 0; i < numRects.length; ++i)
+    for (var i = 0; i < numRectsArray.length; ++i)
     {
-      rg.setNumRectangles(numRects[i]);
+      rg.setNumRectangles(numRectsArray[i]);
       assert.ok(rg.getRightRiemannSum(-5, 0) < HAND_CALCULATED_INTEGRAL_ANSWER,
-        "Correct for " + numRects[i] + " rectangles");
+        "Correct for " + numRectsArray[i] + " rectangles");
     }
   });
