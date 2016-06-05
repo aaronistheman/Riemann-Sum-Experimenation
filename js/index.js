cc43dfa5fff61c4be06125b0eb5a818adf57e78d
aaronistheman <aarons.7007@gmail.com>
2016-06-05 10:36:01 -0700

    By default, linear function data is shown

--- js/index.js
+++ js/index.js
@@ -17,6 +17,11 @@ OptionData.NUM_RECTANGLES = 500000;
 
 $(document).ready(function()
 {
+  // Menu option data instances
+  var linFuncOption = new OptionData("Linear Function", function(x)
+    { return 2 * x + 3; }, -5, 5);
+  
+  // Menu option event handlers
   $("#lin-button").click(function() {
     alert($(this).html());
   });
@@ -29,4 +34,12 @@ $(document).ready(function()
   $("#btwn-button").click(function() {
     alert($(this).html());
   });
+  
+  // By default, display linear function's data
+  $("#display-header").html(linFuncOption.displayHeaer);
+  $("#function").html(linFuncOption.func.toString());
+  $("#range-lower").html(linFuncOption.rangeLow);
+  $("#range-higher").html(linFuncOption.rangeHigh);
+  $("#left-sum").html(linFuncOption.leftSum);
+  $("#right-sum").html(linFuncOption.rightSum);
 });
