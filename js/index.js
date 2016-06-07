7b9cbbc5ff69db607f4d7e8a1180c1696873409d
aaronistheman <aarons.7007@gmail.com>
2016-06-07 10:51:25 -0700

    Arc length menu option works

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ function OptionData(displayHeader, func, rangeLow, rangeHigh)
   this.rangeLow = rangeLow;
   this.rangeHigh = rangeHigh;
   
-  var rg = new RiemannGenerator(func, 500000);
+  var rg = new RiemannGenerator(func, OptionData.NUM_RECTANGLES);
   this.leftSum = rg.getLeftRiemannSum(rangeLow, rangeHigh);
   this.rightSum = rg.getRightRiemannSum(rangeLow, rangeHigh);
 } // custom type OptionData
@@ -25,7 +25,7 @@ function setUpOptionEventHandlers(menuOptionDataArray)
     display(menuOptionDataArray.quadFunc);
   });
   $("#arc-button").click(function() {
-    alert($(this).html());
+    display(menuOptionDataArray.vecFunc);
   });
   $("#btwn-button").click(function() {
     alert($(this).html());
@@ -34,7 +34,7 @@ function setUpOptionEventHandlers(menuOptionDataArray)
 
 function display(optionData)
 {
-  $("#display-header").html(optionData.displayHeaer);
+  $("#display-header").html(optionData.displayHeader);
   $("#function").html(optionData.func.toString());
   $("#range-lower").html(optionData.rangeLow);
   $("#range-higher").html(optionData.rangeHigh);
@@ -42,13 +42,29 @@ function display(optionData)
   $("#right-sum").html(optionData.rightSum);
 }
 
-$(document).ready(function()
+function getMenuOptionData()
 {
   var menuOptionData = {};
+  
   menuOptionData.linFunc = new OptionData("Linear Function", function(x)
     { return 2 * x + 3; }, -5, 5);
   menuOptionData.quadFunc = new OptionData("Quadratic Function", function(x)
     { return 5 * x * x - 3 * x - 3; }, -8, 11);
+    
+  // Arc length vector function needs a little bit more set up
+  var vf = new VectorFunction(function(t) { return -Math.sin(t); },
+    function(t) { return Math.cos(t); },
+    function(t) { return Math.sqrt(t); });
+  menuOptionData.vecFunc = new OptionData(
+    "Magnitude of Vector Function (i.e. Arc Length)",
+    vf.getMagnitudeFunction(), 0, 2 * Math.PI);
+  
+  return menuOptionData;
+}
+
+$(document).ready(function()
+{
+  var menuOptionData = getMenuOptionData();
   
   setUpOptionEventHandlers(menuOptionData);
   
